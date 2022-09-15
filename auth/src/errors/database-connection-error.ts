235a39ddad71a97333214bdda1d7624310f2f27d
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-15 22:55:43 +0200

    adds error handling

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,12 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
 
   reason = "Error connecting to database";
 
   constructor() {
-    super();
+    super("Error Connecting to DB");
 
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
@@ -17,5 +19,4 @@ export class DatabaseConnectionError extends Error {
       },
     ];
   }
-    ]
 }
