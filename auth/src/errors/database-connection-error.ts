2ab838407d465925e379669b5aac8f2f35633a8b
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-15 22:06:34 +0200

    adds init files

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,21 @@
+export class DatabaseConnectionError extends Error {
+  statusCode = 500;
+
+  reason = "Error connecting to database";
+
+  constructor() {
+    super();
+
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+    ]
+}
