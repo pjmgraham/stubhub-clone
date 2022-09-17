991167dea2573dd026a1c088c26390b2c3d83bee
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-17 12:55:25 +0200

    adds signin functionality

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import useRequest from "../../hooks/use-request";
 
 const signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control"></input>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        ></input>
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control"></input>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        ></input>
       </div>
       <br />
-      <button className="btn btn-primary">Sign Up</button>
+      <div>
+        <button className="btn btn-primary">Sign Up</button>
+      </div>
+      <br />
+      {errors}
     </form>
   );
 };
