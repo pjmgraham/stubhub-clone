a524e2e353e43c5edd3100306ad4747ec5cb5ffe
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-17 11:59:11 +0200

    adds signup page

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const signup = () => {
+  return (
+    <form>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input className="form-control"></input>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" className="form-control"></input>
+      </div>
+      <br />
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
+
+export default signup;
