3908c1f649b6822ed1fa4c66a02cf4f529d86e27
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-18 19:15:48 +0200

    adds ticket authentivation logic

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,9 +2,10 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
@@ -32,19 +33,27 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = "test@example.com";
-  const password = "password";
+global.signin = () => {
+  // build jwt payload, {id, email}
+  const payload = {
+    id: "123456789",
+    email: "test@test.com",
+  };
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
+  // create jwt
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = response.get("Set-Cookie");
+  // build session object
+  const session = { jwt: token };
 
-  return cookie;
+  // turn that session into json
+  const sessionJSON = JSON.stringify(session);
+
+  // take json and encode it as base64
+
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // return string that is the cookie with the encoded data
+
+  return [`session=${base64}`];
 };
