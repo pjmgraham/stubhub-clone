d24eb10e7075b142e0319f42b322a2b12e1f29f4
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-19 12:18:57 +0200

    adds nats client

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -12,6 +12,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect("ticketing", "asdf", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
