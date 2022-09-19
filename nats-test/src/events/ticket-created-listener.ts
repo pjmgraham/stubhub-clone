3c309d429091d2ca215da0c48b5618d5a8c2b0d3
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-19 11:07:50 +0200

    adds events files to common module

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,14 +1,18 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = "payments-service";
 
   onMessage(data: any, msg: Message) {
     console.log("Event data!", data);
 
-    
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     msg.ack();
   }
