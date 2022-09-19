2aef1e18216285c3b29aeb0329826f9ea5de5082
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-19 13:13:42 +0200

    adds mock nats wrapper

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -14,6 +14,8 @@ class NatsWrapper {
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
 
+
+
     return new Promise<void>((resolve, reject) => {
       this.client.on("connect", () => {
         console.log("Connected to NATS");
