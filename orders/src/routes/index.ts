a0a2387f290564af614515b90128f6243bf76ae6
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-20 12:31:29 +0200

    adds passing test suite for index route

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,10 +1,15 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
+import { requireAuth } from "@pjmtix/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get('/api/orders', (req: Request, res: Response) => {
-  res.send({});
-}
-)
+router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Order.find({
+    userId: req.currentUser!.id,
+  }).populate("ticket");
+
+  res.send(orders);
+});
 
 export { router as indexOrderRouter };
