5cd418f08af1555a4e5a8d8ba3cf7f4bc0263850
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-23 12:26:23 +0200

    updates common module

--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -3,3 +3,4 @@ import { Publisher, Subjects, TicketUpdatedEvent } from "@pjmtix/common";
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
 }
+
