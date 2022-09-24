638f97a9a93dcb0dbfcfe6f9bae9b49a932b5330
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-24 21:23:05 +0200

    adds dependencies

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,12 +1,8 @@
-import express, { application } from "express";
+import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@pjmtix/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes/index";
-import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -18,11 +14,6 @@ app.use(
   })
 );
 app.use(currentUser);
-
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter)
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
