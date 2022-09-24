9acc95fd92b44c2a97135a771be629c936a4fccc
Patrick Graham <95481954+pjmgraham@users.noreply.github.com>
2022-09-24 18:16:25 +0200

    adds order expiard publiser

---
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -0,0 +1,7 @@
+import { Subjects, Publisher, ExpirationCompleteEvent } from "@pjmtix/common";
+
+export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+}
+
+
